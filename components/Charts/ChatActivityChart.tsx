f2d926688ef8732e0f903938a1a4f754fe919a09
Kirill <kerya132@gmail.com>
2026-02-28 00:10:15 +0300

    feat: modified charts

--- components/Charts/ChatActivityChart.tsx
+++ components/Charts/ChatActivityChart.tsx
@@ -1,29 +1,44 @@
 import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts';
-import { ChartContainer, ChartTooltip, ChartTooltipContent } from '../ui/chart';
-import { ChartComponentProps } from './chart.types';
-//TODO: Make chart min height to a constant
-const AreaSingleChart = ({ chartConfig, chartData }: ChartComponentProps) => (
-  <ChartContainer config={chartConfig} className='flex min-h-[200px] w-full'>
-    <AreaChart accessibilityLayer data={chartData} margin={{ left: 15, right: 15 }}>
-      <CartesianGrid vertical={false} />
-      <XAxis
-        dataKey='date'
-        axisLine={false}
-        interval='preserveStartEnd'
-        tickMargin={5}
-        tickFormatter={value => value.slice(0, 3) + ' ' + value.slice(-4)}
-        minTickGap={10}
-      />
-      <ChartTooltip cursor={false} content={<ChartTooltipContent indicator='line' />} />
-      <Area
-        dataKey='messages'
-        type='natural'
-        fill='var(--color-messages)'
-        fillOpacity={0.4}
-        stroke='var(--color-messages)'
-      />
-    </AreaChart>
-  </ChartContainer>
-);
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '../ui/chart';
+import { ChartDataItem } from '@/types/chart.types';
+import { analyticsPalette } from '@/constants/palletes';
+import { cn } from '@/lib/utils';
 
-export default AreaSingleChart;
+interface ChatActivityChartProps {
+  chartConfig: ChartConfig;
+  chartData: ChartDataItem[];
+  className?: string;
+}
+
+const ChatActivityChart = ({ chartConfig, chartData, className }: ChatActivityChartProps) => {
+  const usersList = Object.keys(chartData[0]).filter(property => property !== 'date' && property !== 'totalMessages');
+
+  return (
+    <ChartContainer config={chartConfig} className={cn('flex w-full', className)}>
+      <AreaChart accessibilityLayer data={chartData} margin={{ left: 15, right: 15 }}>
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey='date'
+          axisLine={false}
+          interval='preserveStartEnd'
+          tickMargin={5}
+          tickFormatter={value => value.slice(0, 3) + ' ' + value.slice(-4)}
+          minTickGap={10}
+        />
+        <ChartTooltip cursor={false} content={<ChartTooltipContent indicator='line' />} />
+        {usersList.map((user, index) => (
+          <Area
+            dataKey={user}
+            type='monotone'
+            fill={analyticsPalette[index]}
+            fillOpacity={0.4}
+            stroke={analyticsPalette[index]}
+            key={user}
+          />
+        ))}
+      </AreaChart>
+    </ChartContainer>
+  );
+};
+
+export default ChatActivityChart;
--- components/Charts/chart.types.ts
+++
@@ -1,6 +0,0 @@
-import { ChartConfig } from '../ui/chart';
-
-export interface ChartComponentProps {
-  chartConfig: ChartConfig;
-  chartData: object[];
-}
